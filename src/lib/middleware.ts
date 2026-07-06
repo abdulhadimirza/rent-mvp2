7dc8ebb0a0c0082d0d6b80593e3b203fd8c2e2fe
Abdul Hadi Mirza <56407595+gitstorm1@users.noreply.github.com>
2026-07-06 20:15:28 +0500

    Redirect to dashboard by default

--- src/lib/middleware.ts
+++ src/lib/middleware.ts
@@ -56,7 +56,7 @@ export async function updateSession(request: NextRequest) {
     ) {
         // user is already logged in, redirect to dashboard
         const url = request.nextUrl.clone();
-        url.pathname = '/upload';
+        url.pathname = '/dashboard';
         return NextResponse.redirect(url);
     }
 
