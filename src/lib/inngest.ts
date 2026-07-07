7ce00eac5cac65481a30c98b7e227b303a7132d1
Abdul Hadi Mirza <56407595+gitstorm1@users.noreply.github.com>
2026-07-07 07:24:13 +0500

    Attempt to fix production inngest error

--- src/lib/inngest.ts
+++ src/lib/inngest.ts
@@ -1,3 +1,6 @@
 import { Inngest } from 'inngest';
 
-export const inngest = new Inngest({ id: 'rent-mvp2' });
+export const inngest = new Inngest({ 
+    id: 'rent-mvp2',
+    isDev: process.env.NODE_ENV === 'development'
+});
